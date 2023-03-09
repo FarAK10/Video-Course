2019a1bd763d64742be8d86e8a71bbc0cdd02231
FarAK10 <botirovfarhod115@gmail.com>
2023-03-09 10:15:13 +0500

    feat: set up eslint prettier

---
+++ video-course/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  printWidth: 100,
+  plugins: [
+    './node_modules/prettier-plugin-multiline-arrays',
+  ],
+};
