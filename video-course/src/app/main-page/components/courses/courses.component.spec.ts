634ebc4f331fbb1eb000fcd53c4eae84ff5075f8
FarAK10 <botirovfarhod115@gmail.com>
2023-03-13 11:07:16 +0500

    feat: add required modules for unit testing

--- video-course/src/app/main-page/components/courses/courses.component.spec.ts
+++ video-course/src/app/main-page/components/courses/courses.component.spec.ts
@@ -2,8 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from '../course/course.component';
+
 import { MatIconModule } from '@angular/material/icon';
+
 import { DurationPipe } from 'src/app/shared/pipes/duration-pipe/duration.pipe';
+import { FilterPipe } from 'src/app/shared/pipes/filter-pipe/filter.pipe';
+import { OrderByPipe } from 'src/app/shared/pipes/orderBy-pipe/order-by.pipe';
+
 import { HighlightDirective } from 'src/app/shared/directives/highlight.directive';
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
@@ -16,7 +21,9 @@ describe('CoursesComponent', () => {
         CourseComponent,
         HighlightDirective,
         DurationPipe,
+        OrderByPipe,
       ],
+      providers: [FilterPipe],
       imports: [MatIconModule],
     }).compileComponents();
 
