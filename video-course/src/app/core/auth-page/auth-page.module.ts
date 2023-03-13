22991c1b70b7bd7a8e3af7232fef0ca049b7fe13
FarAK10 <botirovfarhod115@gmail.com>
2023-03-13 23:55:11 +0500

    feat: add login component

--- video-course/src/app/core/auth-page/auth-page.module.ts
+++ video-course/src/app/core/auth-page/auth-page.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     LoginComponent,
     SignupComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   exports: [
     LoginComponent,
     SignupComponent,
--- video-course/src/app/core/auth-page/components/login/login.component.ts
+++ video-course/src/app/core/auth-page/components/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  hide = true;
 
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    console.log;
+    if (this.loginForm.valid) {
+      const login = this.loginForm.value;
+    }
+  }
 }
